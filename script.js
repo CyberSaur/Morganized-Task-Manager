d8a6348ad1686ea3b4616c74140a2ae36182a252
Seth Hewage <seth_hewage@Seths-Mac-mini.local>
2021-09-16 08:12:19 -0700

    Just started the javascript part of the project. Created the buttons' basic functionalities. Also, updated the button id's in the html file.

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-/******************************************************************
+/***********************************************************************
 Programmer: Suneth Hewage
-Description: This is the javascript file of the to-do list web app.
-******************************************************************/
+Description: This is the javascript file of the Morganized Task Manager.
+***********************************************************************/
 
+// Variable declaration.
+var add_btn = document.getElementById("btn_add");
+var erase_btn = document.getElementById("btn_erase");
+var delete_btn = document.getElementById("btn_delete");
+var save_btn = document.getElementById("btn_save");
 
+// Listening to user's actions.
+add_btn.addEventListener("click", addTaskToList);
+erase_btn.addEventListener("click", eraseCompletedTasks);
+delete_btn.addEventListener("click", deleteList);
+save_btn.addEventListener("click", saveList);
 
-
-
-
-
+// Functions to do the job.
+function addTaskToList() {
+    alert("Hi, you just clicked the add a task button!");
+}
+function eraseCompletedTasks() {
+    alert("Hi, you just clicked the erase completed tasks button!")
+}
+function deleteList() {
+    alert("Hi, you just clicked the delete your list button!");
+}
+function saveList() {
+    alert("Hi, you just clicked the save your list button!");
+}
 
 
 
