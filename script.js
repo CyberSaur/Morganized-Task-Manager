df0cb8f53400a3076bda9e8c603100d878c571a7
Seth Hewage <seth_hewage@Seths-Mac-mini.local>
2021-09-17 19:09:05 -0700

    Developed the add a task to list functionality. Also, modified the html file a bit. Just changed the ol tag to ul tag. That's all.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ var add_btn = document.getElementById("btn_add");
 var erase_btn = document.getElementById("btn_erase");
 var delete_btn = document.getElementById("btn_delete");
 var save_btn = document.getElementById("btn_save");
+var task_txt = document.getElementById("txt_task");
+var task_lst = document.getElementById("lst_task");
 
 // Listening to user's actions.
 add_btn.addEventListener("click", addTaskToList);
@@ -18,6 +20,8 @@ save_btn.addEventListener("click", saveList);
 // Functions to do the job.
 function addTaskToList() {
     alert("Hi, you just clicked the add a task button!");
+    var task_name = task_txt.value;
+    newTask(task_name, false);
 }
 function eraseCompletedTasks() {
     alert("Hi, you just clicked the erase completed tasks button!")
@@ -28,7 +32,18 @@ function deleteList() {
 function saveList() {
     alert("Hi, you just clicked the save your list button!");
 }
-
+function newTask(task_param, completed_param) {
+    var task_unit = document.createElement("li");
+    var task_name = document.createTextNode(task_param);
+    task_unit.appendChild(task_name);
+    
+    if (completed_param) {
+        task_unit.classList.add("completed");
+    }
+    
+    task_lst.appendChild(task_unit);
+    task_unit.addEventListener("dblclick", toggleTaskStatus);
+}
 
 
 
