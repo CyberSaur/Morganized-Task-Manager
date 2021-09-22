b94ef5b6719f282bdd39715630aa7f00f5d07a89
Seth Hewage <seth_hewage@Seths-Mac-mini.local>
2021-09-22 10:13:37 -0700

    Finished the save user's list part. Also developed the load list part. All the features are implemented now. However, there's an error in save list part. As a result the list won't load. We need to fix this issue.

--- script.js
+++ script.js
@@ -14,23 +14,14 @@ var task_lst = document.getElementById("lst_task");
 // Listening to user's actions.
 add_btn.addEventListener("click", addTaskToList);
 erase_btn.addEventListener("click", eraseCompletedTasks);
-delete_btn.addEventListener("click", deleteList);
-save_btn.addEventListener("click", saveList);
+delete_btn.addEventListener("click", deleteTaskList);
+save_btn.addEventListener("click", saveTaskList);
 
 // Functions to do the job.
 function addTaskToList() {
     alert("Hi, you just clicked the add a task button!");
     var task_name = task_txt.value;
     newTask(task_name, false);
-}
-function eraseCompletedTasks() {
-    alert("Hi, you just clicked the erase completed tasks button!")
-}
-function deleteList() {
-    alert("Hi, you just clicked the delete your list button!");
-}
-function saveList() {
-    alert("Hi, you just clicked the save your list button!");
 }
 function newTask(task_param, completed_param) {
     var task_unit = document.createElement("li");
@@ -51,31 +42,50 @@ function toggleTaskStatus() {
         this.classList.add("completed");
     }
 }
+
 function eraseCompletedTasks() {
+    alert("Hi, you just clicked the erase completed tasks button!");
     var completed_tasks = task_lst.getElementsByClassName("completed");
     
     while (completed_tasks.length > 0) {
         completed_tasks.item(0).remove();
     }
 }
-function deleteList() {
+
+function deleteTaskList() {
+    alert("Hi, you just clicked the delete your list button!");
     var tasks = task_lst.children;
     while (tasks.length > 0) {
         tasks.item(0).remove();
     }
 }
 
+function saveTaskList() {
+    alert("Hi, you just clicked the save your list button!");
+    var task_arr = [];
+    
+    for (var i = 0; i < task_lst.children.length; i++) {
+        var task = task_lst.children.item(i);
+        
+        var task_details = {
+            "toDo": task.innerText,
+            "completed": task.classList.contains("completed")
+        };
+        
+        task_arr.push(task_details);
+    }
+    localStorage.setItem("task_arr", JSON.stringify(task_arr));
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+function loadTaskList() {
+    if (localStorage.getItem("task_arr") != null) {
+        var task_arr = JSON.parse(localStorage.getItem("task_arr"));
+        
+        for(var i = 0; i < task_arr.length; i++) {
+            var task = task_arr[i];
+            new_task(task.toDo, task.completed);
+        }
+    }
+}
+loadTaskList();
 
